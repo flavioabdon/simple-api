c98403ddbf268ffcae71d01e26c76afe48c9e23e
flavioabdon <flaviocondori@gmail.com>
2025-07-02 12:19:58 -0400

    Primer commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hola, el servidor está vivo 🚀');
+});
+
+app.get('/stress', (req, res) => {
+  // Simula una carga artificial de CPU
+  const end = Date.now() + 1000; // 1 segundo
+  while (Date.now() < end) {}
+  res.send('Carga procesada 🧠');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
